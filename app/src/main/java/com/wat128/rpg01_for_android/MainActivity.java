d5d751538fa64fbc53a118c3c3342c871b0506a3
wat128 <wat128sideb@gmail.com>
2020-08-19 15:53:55 +0900

    ボタン押下中は継続して移動する - 上下左右

--- app/src/main/java/com/wat128/rpg01_for_android/MainActivity.java
+++ app/src/main/java/com/wat128/rpg01_for_android/MainActivity.java
@@ -14,48 +14,98 @@ public class MainActivity extends AppCompatActivity {
 
     final float MARGIN = 10.0f;
 
-    private TextView textView;
+    private enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
 
-    private Handler handler;
-    private boolean isPress;
+    private TextView _textView;
+    private Handler _handler;
+    private boolean _pressed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text);
+        _textView = findViewById(R.id.text);
 
         Button buttonUp = findViewById(R.id.up);
-        buttonUp.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
+        buttonUp.setOnTouchListener(_ButtonListener);
 
-                switch(event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        isPress = true;
+        Button buttonDown = findViewById(R.id.down);
+        buttonDown.setOnTouchListener(_ButtonListener);
 
-                        handler = new Handler();
-                        final Runnable runnable = new Runnable() {
-                            @Override
-                            public void run() {
-                                if(!isPress)
-                                    return;
+        Button buttonLeft = findViewById(R.id.left);
+        buttonLeft.setOnTouchListener(_ButtonListener);
 
-                                final float currentY = textView.getTranslationY();
-                                textView.setTranslationY( currentY - MARGIN);
+        Button buttonRight = findViewById(R.id.right);
+        buttonRight.setOnTouchListener(_ButtonListener);
+    }
 
-                                handler.postDelayed(this, 10L);
-                            }
-                        };
-                        handler.post(runnable);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        isPress = false;
-                        break;
-                }
-                return false;
+    private View.OnTouchListener _ButtonListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+
+            final int action = event.getAction();
+
+            switch (v.getId()) {
+                case R.id.up:       move(Direction.Up, action);     break;
+                case R.id.down:     move(Direction.Down, action);   break;
+                case R.id.left:     move(Direction.Left, action);   break;
+                case R.id.right:    move(Direction.Right, action);  break;
             }
-        });
+
+            return false;
+        }
+    };
+
+    private void move(final Direction direction, final int action) {
+
+        switch(action){
+            case MotionEvent.ACTION_DOWN:
+                _pressed = true;
+
+                _handler = new Handler();
+                final Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        if(!_pressed)
+                            return;
+
+                        final float curPos;
+                        switch (direction) {
+                            case Up:
+                                curPos = _textView.getTranslationY();
+                                _textView.setTranslationY( curPos - MARGIN);
+                                break;
+                            case Down:
+                                curPos = _textView.getTranslationY();
+                                _textView.setTranslationY( curPos + MARGIN);
+                                break;
+                            case Left:
+                                curPos = _textView.getTranslationX();
+                                _textView.setTranslationX( curPos - MARGIN);
+                                break;
+                            case Right:
+                                curPos = _textView.getTranslationX();
+                                _textView.setTranslationX( curPos + MARGIN);
+                                break;
+                        }
+
+                        _handler.postDelayed(this, 10L);
+                    }
+                };
+
+                _handler.post(runnable);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                _pressed = false;
+                break;
+        }
     }
 }
